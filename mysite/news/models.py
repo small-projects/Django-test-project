6c451d20a7136c161e9511232afa49afcdaa260a
Dmitry <discover95@mail.ru>
2020-10-18 21:16:20 +0300

    Добавлена модель News

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+Class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
