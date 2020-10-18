a026d163e39caa5bb3962401ca7dd9f590d63d16
Dmitry <discover95@mail.ru>
2020-10-18 23:10:12 +0300

    MEDIA_ROOT

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-Class News(models.Model):
+class News(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
